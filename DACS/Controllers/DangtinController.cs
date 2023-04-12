2675a386031c2752d04f887cb957886fe504d871
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-12 15:44:59 +0700

    dsfds

---
+++ DACS/Controllers/DangtinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DACS.Controllers
+{
+    public class DangtinController : Controller
+    {
+        // GET: Dangtin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DACS/Scripts/JavaScript.js
@@ -0,0 +1,40 @@
+﻿//// get the hamburger menu element and the slide menu element by ID
+//const hamburger = document.querySelector('#mogi-slide-menu .navbar-hamburger');
+//const slideMenu = document.querySelector('#mogi-slide-menu .hamburger-menu');
+
+//// add a click event listener to the hamburger menu icon
+//hamburger.addEventListener('click', () => {
+//    // toggle the 'active' class to show/hide the slide menu
+//    slideMenu.classList.toggle('active');
+//});
+
+//// get the close icon element by class name
+//const closeIcon = slideMenu.querySelector('.close-icon');
+
+//// add a click event listener to the close icon
+//closeIcon.addEventListener('click', () => {
+//    // remove the 'active' class to hide the slide menu
+//    slideMenu.classList.remove('active');
+//});
+const hamburgerBtn = document.querySelector('.navbar-hamburger i');
+const slideMenu = document.querySelector('.hamburger-menu');
+const slideOverlay = document.querySelector('#mogi-slide-overlay');
+
+function openSlideMenu() {
+    slideMenu.style.left = '0';
+    slideOverlay.style.display = 'block';
+}
+
+function closeSlideMenu() {
+    slideMenu.style.left = 'calc(-100vw - 100px)';
+    slideOverlay.style.display = 'none';
+}
+
+hamburgerBtn.addEventListener('click', openSlideMenu);
+slideOverlay.addEventListener('click', closeSlideMenu);
+
+
+
+
+
+
